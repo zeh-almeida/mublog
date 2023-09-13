bfe8a5c4e0f56614a01adc9e44116a7f89da7915
José Ricardo Prado de Almeida <zehpavora@gmail.com>
2023-09-13 16:02:47 -0300

    Persisting font choice

--- src/js/fonts.js
+++ src/js/fonts.js
@@ -1,14 +1,27 @@
 const FONT_SELECTOR = "font-selection";
 const FONT_SUFFIX = "_font";
+const FONT_STORAGE_NAME = "selected-font";
 
-function select_font(event) {
-  console.log(event.target.value);
+function set_current_font(fontName) {
+  if (!fontName) {
+    return;
+  }
 
   [...document.documentElement.classList]
     .filter((c) => c.endsWith(FONT_SUFFIX))
     .map((c) => document.documentElement.classList.remove(c));
 
-  document.documentElement.classList.add(`${event.target.value}${FONT_SUFFIX}`);
+  document.documentElement.classList.add(`${fontName}${FONT_SUFFIX}`);
+  localStorage.setItem(FONT_STORAGE_NAME, fontName);
+}
+
+function select_font(event) {
+  set_current_font(event.target.value);
+}
+
+function check_font_selection() {
+  const fontName = localStorage.getItem(FONT_STORAGE_NAME);
+  set_current_font(fontName);
 }
 
 document.addEventListener(
@@ -19,3 +32,5 @@ document.addEventListener(
   },
   true
 );
+
+document.addEventListener("DOMContentLoaded", check_font_selection, true);
